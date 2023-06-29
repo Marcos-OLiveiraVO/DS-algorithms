12adc7e06d0a6bda3d185c15d26354aac4693cd9
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-06-29 01:04:58 -0300

    queue estructure

---
+++ queue.js
@@ -0,0 +1,29 @@
+class Queue {
+  constructor() {
+    this.item = [];
+  }
+
+  enqueue() {
+    // adiciona um novo item
+  }
+
+  dequeue() {
+    //remove um item
+  }
+
+  front() {
+    //retorna o primeiro elemento da fila
+  }
+
+  isEmpty() {
+    //verifica se a fila está vazia ou não
+  }
+
+  size() {
+    //verifica o tamanho da fila
+  }
+
+  print() {
+    // imprimi a fila no console
+  }
+}
