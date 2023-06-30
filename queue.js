b1586d865f34d9f128ba6ac7376eb0209fa2f347
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-06-30 01:00:39 -0300

    Priority queue

--- queue.js
+++ queue.js
@@ -34,13 +34,59 @@ class Queue {
   }
 }
 
-const queue = new Queue();
+// const queue = new Queue();
 
-queue.enqueue("carlos");
-queue.enqueue("joao");
-// queue.dequeue();
-// console.log(queue.front());
-console.log(queue.size());
-console.log(queue.isEmpty());
+// queue.enqueue("carlos");
+// queue.enqueue("joao");
+// // queue.dequeue();
+// // console.log(queue.front());
+// console.log(queue.size());
+// console.log(queue.isEmpty());
 
-// console.log(queue.print());
+// // console.log(queue.print());
+
+class Node {
+  constructor(element, priority) {
+    this.element = element;
+    this.priority = priority;
+  }
+}
+
+class PriorityQueue {
+  constructor() {
+    this.items = [];
+  }
+
+  queue(element, priority) {
+    let added = false;
+    const newNode = new Node(element, priority);
+
+    for (let i = 0; i < this.items.length; i++) {
+      if (newNode.priority < this.items[i].priority) {
+        this.items.splice(i, 0, newNode);
+        added = true;
+        break;
+      }
+    }
+    if (!added) {
+      this.items.push(newNode);
+    }
+  }
+
+  dequeue() {
+    return this.items.shift();
+  }
+
+  print() {
+    for (let i = 0; i < this.items.length; i++) {
+      console.log(
+        `${this.items[i].element}, prioridade: ${this.items[i].priority}`
+      );
+    }
+  }
+}
+
+const pQueue = new PriorityQueue();
+
+pQueue.queue("lucas", 2);
+pQueue.print();
