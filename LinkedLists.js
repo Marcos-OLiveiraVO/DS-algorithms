f2bbd3c0dbd4a8afa02e9af7b1adb690921629b0
Marcos Oliveira <88260644+Marcos-OLiveiraVO@users.noreply.github.com>
2023-08-01 22:53:09 -0300

    insertAt

--- LinkedLists.js
+++ LinkedLists.js
@@ -31,6 +31,27 @@ class LinkedList {
   }
 
   insert(position, element) {
+    if (position >= 0 && position <= this.length) {
+      let node = new Node(element);
+      current = this.head;
+      let previous;
+      let index = 0;
+
+      if (position === 0) {
+        node.next = current;
+        head = node;
+      } else {
+        while (index++ < position) {
+          previous = current;
+          current = current.next;
+        }
+        node.next = current;
+        previous.next = node;
+      }
+      length++;
+      return true;
+    }
+
     // add a element in to a specific position
   }
 
@@ -47,14 +68,15 @@ class LinkedList {
           previous = current;
           current = current.next;
         }
-        previous.next = current.next;
+        node.next = current;
+        previous.next = node;
       }
-      this.length--;
-      return current.element;
+      this.length++;
+      return true;
     } else {
-      return null;
+      return false;
     }
-    // remove a element from especific position
+    // remove a element from specific position
   }
 
   remove(element) {
